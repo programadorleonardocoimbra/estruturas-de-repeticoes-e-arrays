8e93fed448d6869f0474393b38548b0967657375
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-03-20 19:26:39 -0300

    alt_GABARITO03

--- GabaritoOficial/GabSomaDeElementos.java
+++ GabaritoOficial/GabSomaDeElementos.java
@@ -1,20 +1,20 @@
 package GabaritoOficial;
 
 //1. Soma de elementos:
-  //Crie um programa que leia 5 números inteiros, armazene em um array e mostre a soma total.
-
+    //  Crie um programa que leia 5 números inteiros, armazene em um array e mostre a soma total.
+       //  Ideia: usar o for para percorrer o array e acumular valores.
 
 public class GabSomaDeElementos {
     public static void main(String[] args) {
-        int[] v = new int[5];
-        int soma = 0;
+        int[] v = new int[5]; // Cria um array com 5 posições
+        int soma = 0; // Variável acumuladora
 
-        for(int i = 0; i < 5; i++) {
-            v[i] = i + 1;
-            soma += v[i];
+        for(int i = 0; i < 5; i++) { // Loop para percorrer o array
+            v[i] = i + 1; // Atribui valores (1+2+3+4+5 = 15)
+            soma += v[i]; // Soma cada elemento ao total
 
         }
-        System.out.println("Soma é: " + soma);
+        System.out.println("Soma é: " + soma); //Imprime a RESPOSTA
 
 
     }
