1bafe2b200723c5fb551e0d3da50e13b2a51f0a7
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-03-20 20:52:28 -0300

    alt_Gabarito 07

---
+++ GabaritoOficial/GabInverterArray.java
@@ -0,0 +1,23 @@
+package GabaritoOficial;
+
+//5. Inverter array
+    //  Leia 6 números e exiba o array em ordem inversa. 
+        //  Ideia: percorrer o array ao contrário.
+
+public class GabInverterArray {
+    public static void main(String[] args) {
+        int[] v = {1,2,3,4,5,6}; // Array inicial
+
+    for(int i = v.length - 1; i >= 0; i--){ // Começa do último índice   
+        System.out.print (v[i] + " "); // imprimir de trás para frente
+
+
+    }
+
+
+    }
+
+
+
+    
+}
